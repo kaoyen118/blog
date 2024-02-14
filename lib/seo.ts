f9660535e0660a742378a8812dca87481199b514
liulekang <liulekang@syriusrobotics.com>
2024-02-14 04:29:17 +0100

    modified:   lib/seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
   title: 'KaoYen Blog |  Tourism expert, overseas study consultant, international trade manager, rationalist, engineer',
   description:
-    'Hi, I'm kaoyen, a Traveler, Study Abroad Consultant and International Trade Manager. I focus on rational thinking and am good at analyzing and executing. My background as an engineer allows me to find innovative solutions in various situations and put them into practice.',
+    "Hi, I'm kaoyen, a Traveler, Study Abroad Consultant and International Trade Manager. I focus on rational thinking and am good at analyzing and executing. My background as an engineer allows me to find innovative solutions in various situations and put them into practice.",
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://blog-xi-lovat-96.vercel.app/'
       : 'http://localhost:3000'
   ),
 } as const
