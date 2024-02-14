c615ce31e648e4000cb619cd517d6dbe6dd39faa
liulekang <liulekang@syriusrobotics.com>
2024-02-14 05:00:16 +0100

    modified:   lib/seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -4,7 +4,7 @@ export const seo = {
     "Hi, I'm kaoyen, a Traveler, Study Abroad Consultant and International Trade Manager. I focus on rational thinking and am good at analyzing and executing. My background as an engineer allows me to find innovative solutions in various situations and put them into practice.",
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://blog-xi-lovat-96.vercel.app/'
+      ? 'https://blog-xi-lovat-96.vercel.app'
       : 'http://localhost:3000'
   ),
 } as const
