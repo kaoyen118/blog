4c911f36dac35f0eed9b4ee592ecb490f0ae3280
liulekang <liulekang@syriusrobotics.com>
2024-02-14 05:32:47 +0100

    modified:   app/(main)/ama/page.tsx

--- app/(main)/ama/page.tsx
+++ app/(main)/ama/page.tsx
@@ -8,9 +8,9 @@ import AlipayQR from './alipay-qr.jpg'
 import ThankYouLetterScreenshot1 from './Arc aagD26w9@2x.png'
 import ThankYouLetterScreenshot2 from './Arc ynleUdHy@2x.png'
 
-const title = 'AMA 一对一咨询'
+const title = 'AMA / Ask Me Anything'
 const description =
-  'Cali 提供一对一的咨询服务（Ask Me Anything）。我有前端开发、全栈开发、UI/UX 设计、创业经验、英语技能、美国留学与工作、内容创作等经验，可以为你解答相关的问题，我也支持纯英语沟通。'
+  'Kaoyen offers one-on-one consultation (Ask Me Anything). I have extensive experience in the fields of tourism, study abroad consulting, international trade, rationalist thinking, and engineering background. If you have questions related to travel, study abroad, foreign trade, rational thinking or engineering skills, please consult. Both Chinese and English are available, and we look forward to sharing our experiences and insights with you.'
 
 export const metadata = {
   title,
@@ -31,7 +31,7 @@ export default function AskMeAnythingPage() {
     <Container className="mt-16 sm:mt-24">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Ask Me Anything / 一对一咨询
+          Ask Me Anything / Ask Me Anything
         </h1>
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
@@ -39,45 +39,24 @@ export default function AskMeAnythingPage() {
       </header>
 
       <article className="prose dark:prose-invert">
-        <h2>咨询内容</h2>
-        <p>我可以为你解答以下相关的问题：</p>
+        <h2>Contents of Cosultant and service</h2>
+        <p>I can answer the following questions for you:</p>
         <ul>
           <li>
-            <b>前端/全栈开发</b>
-            ：工作难找，或是寻求职场建议？想提升自己的工程师水平？还是想锐评
-            React vs Vue？
+            <b>Travel Tips</b>
+            ：First time to travel abroad inexperienced? Language barrier can't communicate? Is it inconvenient to call a car in the hotel? Not easy to exchange foreign currency?
           </li>
           <li>
-            <b>UI/UX 设计</b>：不知道如何开始学习设计？想提升自己的设计水平？
+            <b>Study abroad application Advice</b>：Choosing a country is difficult? Having trouble choosing a school? Don't understand the process of applying to study abroad? Incomplete preparation of materials? Don't know how to pay tuition in foreign currency?
           </li>
           <li>
-            <b>创业经验</b>
-            ：我是
-            <RichLink href="https://zolplay.com" target="_blank">
-              佐玩
-            </RichLink>
-            的创始人，现在带领着团队帮助游戏开发者打造协同平台。
+            <b>International Trade Related</b>
+            ：Choosing a country is difficult? Having trouble choosing a school? Don't understand the process of applying to study abroad? Incomplete preparation of materials? Don't know how to pay tuition in foreign currency?
           </li>
           <li>
-            <b>英语技能</b>：英语能力不足，想提高自己的英语水平？想跟 Cali
-            一样可以{' '}
-            <RichLink
-              href="https://y.qq.com/n/ryqq/songDetail/003c0O9B3firAU"
-              favicon={false}
-              target="_blank"
-            >
-              英文说唱
-            </RichLink>
-            ？
-          </li>
-          <li>
-            <b>其他</b>
-            ：我的美国留学与工作经验，内容创作，音乐制作经验等等都可以帮助你解答一些其他的问题。
+            <b>International Trade Related</b>：Foreign trade transport problems? Having trouble clearing customs?
           </li>
         </ul>
-        <p>
-          你也可以全程跟我用英语聊，我也非常乐意，因为可能我的英语比中文说的要好。。。
-        </p>
 
         <h2>定价</h2>
         <p>我的一对一咨询的价格为：</p>
@@ -93,21 +72,12 @@ export default function AskMeAnythingPage() {
         <p className="flex justify-center md:block md:justify-start">
           <span className="inline-flex flex-col items-center">
             <Image src={AlipayQR} alt="" className="w-44 dark:brightness-90" />
-            <span className="mt-1 text-sm font-medium">支付宝二维码</span>
+            <span className="mt-1 text-sm font-medium">Wechat QRCode</span>
           </span>
         </p>
-        <p>
-          一旦你完成支付，通过{' '}
-          <RichLink
-            href="https://cal.com/calicastle/ask-me-anything"
-            target="_blank"
-          >
-            这个链接
-          </RichLink>
-          来跟我预约一个合适你的时间。
-        </p>
+        
 
-        <h2>感谢信</h2>
+        {/* <h2>感谢信</h2>
         <p>
           下面两个截图摘选自两名 Twitter
           朋友的私信，能够帮助到更多的人一直是我的使命：
@@ -123,7 +93,7 @@ export default function AskMeAnythingPage() {
             alt=""
             className="max-w-full"
           />
-        </p>
+        </p> */}
       </article>
     </Container>
   )
